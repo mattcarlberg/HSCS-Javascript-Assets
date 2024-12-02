9005618abb2182f6a55577818d22c826d4701c9a
mattcarlberg <mattcarlberg@yahoo.com>
2024-12-02 10:31:09 -0500

    Create prompt.js

---
+++ prompt.js
@@ -0,0 +1,13 @@
+console.log("running")
+
+
+// //Idea 1 - prompts 
+let fn = prompt("what's your first name?")
+let ln = prompt("what's your last name?")
+console.log(`Nice to meet you ${fn} ${ln}.`)
+
+//Idea 2 - prompts with conversion to numbers 
+let x = Number(prompt("pick a number"))
+let y = Number(prompt("pick another number"))
+let sum = x + y
+console.log(`${x} plus ${y} is ${sum}`)
