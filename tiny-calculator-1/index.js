5d1f8d5f114cdb2daa07f7e7dfed5d30470618d8
mattcarlberg <mattcarlberg@yahoo.com>
2024-12-10 09:44:48 -0500

    Update index.js
    Separating EVERYTHING out into its own function.

--- tiny-calculator-1/index.js
+++ tiny-calculator-1/index.js
@@ -1,39 +1,42 @@
 let output_str = ""; 
 
 function setup(){
-  document.getElementById("1btn").addEventListener("click", numberClicked)
-  document.getElementById("2btn").addEventListener("click", numberClicked)
-  document.getElementById("3btn").addEventListener("click", numberClicked)
+  document.getElementById("1btn").addEventListener("click", oneClicked)
+  document.getElementById("2btn").addEventListener("click", twoClicked)
+  document.getElementById("3btn").addEventListener("click", threeClicked)
 
   document.getElementById("sq-btn").addEventListener("click", go_sq)
   document.getElementById("sqrt-btn").addEventListener("click", go_sqrt)
 
 }
 
-function numberClicked(event){
-  if(event.target.id == "1btn"){
+function oneClicked(event){
     output_str = output_str + "1"
-  }
-  if(event.target.id == "2btn"){
-    output_str = output_str + "2"
-  }
-  if(event.target.id == "3btn"){
-    output_str = output_str + "3"
-  }
-  document.getElementById("ans-text").innerHTML = output_str
+    document.getElementById("ans-text").innerHTML = output_str
 }
+
+function twoClicked(event){
+    output_str = output_str + "2"
+    document.getElementById("ans-text").innerHTML = output_str
+}
+
+function threeClicked(event){
+    output_str = output_str + "3"
+    document.getElementById("ans-text").innerHTML = output_str
+}
+  
 
 function go_sq(event){
   let num = Number(output_str)
   let result = num*num 
-  output_str = output_str + " = " + result
+  output_str = output_str + ` -> ${result}`
   document.getElementById("ans-text").innerHTML = output_str
 }
 
 function go_sqrt(event){
   let num = Number(output_str)
   let result = Math.sqrt(num) 
-  output_str = output_str + " = " + result
+  output_str = output_str + ` -> ${result}`
   document.getElementById("ans-text").innerHTML = output_str
 }
 
